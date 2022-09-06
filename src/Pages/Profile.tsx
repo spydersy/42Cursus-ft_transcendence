5104b03bf924c53e2c87a3818956e2d409effca0
Elmahdi Elaazmi <eelaazmi@e1r7p2.1337.ma>
2022-09-06 22:46:40 +0100

    [add] other tabs on profile + check notes

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -10,114 +10,22 @@ import  Badge4 from "../assets/imgs/avatar/a6.png";
 import  Badge5 from "../assets/imgs/avatar/a4.png";
 import  Badge6 from "../assets/imgs/avatar/a5.png";
 
-import Img from "../assets/imgs/avatar/a1.png";
-
 import  B1 from "../assets/imgs/badge1.svg";
 import  B2 from "../assets/imgs/badge2.svg";
 import  B3 from "../assets/imgs/badge3.svg";
 
 import { PlayerCard , UserCard} from '../components/PlayerProfile';
-
-import{ReactComponent as ScoreBoard }from "../assets/imgs/SC1.svg"
-import{ReactComponent as Sc }from "../assets/imgs/ScoreBoard.svg"
-import {ReactComponent as Timer }from "../assets/imgs/timer.svg"
 import {ReactComponent as AddIcon} from "../assets/imgs/add-icon.svg";
-
 import {ReactComponent as Etimer} from "../assets/imgs/Etimer.svg";
 /////
 
 const Backcolor = "#533483"
 const Barside = "#f3460fe"
 const GreyBackcolor = "#282c34"
-const Borderimgcolor = "white";
 const ProgressUser = "40%"
 const player = { name: "Alchemist", login: "Eelaazmi", lvl: "1", gamePlayed : 350, lost : 150,  won : 200, rank: "rank1"}
 
-////
-
-const TheBox = styled.div`
-  padding: 10p;
-  width: 100%;
-  border: 0px solid ${props => props.theme.colors.primarybg};
-`;
-
-const ProgressBar = styled.div`
-width: 100%;
-height: 32px;
-background-color: ${GreyBackcolor};
-border-radius: 18px;
-display: flex;
-align-items: center;
-margin: 15px 0px;
-
-
-> div{
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: flex-end;
-  height: 100%;
-  width: ${ProgressUser};
-  background-color:${Backcolor};
-  border-radius: 15px;
-
-  .lvl{
-    margin-right: 18px;
-    font-size: ${props => props.theme.fontSize.s};
-    font-weight: 12px;
-    color: white;
-    -webkit-text-stroke: 1px #b2a2b69a;
-  }
-
-}
-`
-//// DefaultComp
-export default function Profile() {
-  return (
-    <div className='container' style={{marginTop: "50px"}}>
-          <TheBox>
-              <PlayerCard player={player} />
-              <ProgressBar>
-                <div >
-                  <div className='lvl'>
-                    level 8 - {ProgressUser}
-                  </div>
-                </div>
-              </ProgressBar>
-          </TheBox>
-          <PlayerTabsBar/>
-    </div>
-  )
-};
-
-///// PlayerTabs Section
-export function PlayerTabsBar()
-{
-  const [index, setindex] = useState(1)
-  return ( 
-    <PlayerAchieveStyle>
-      <Navlinks  index={index} setindex={(e)=> setindex(e)} list={linkslist}/> 
-        {index === 0 && <TabOne/> }
-        {index === 1 && <Tabtwo/>}
-        {index === 2 && <Tabthree/>}
-    </PlayerAchieveStyle>
-  )
-}
-
-const PlayerAchieveStyle = styled.div`
-  padding-top: 20px;
-  margin : 0px 0px;
-  width:  100%;
-  align-items: center;
-  height: auto;
-  border: 1px solid ${props => props.theme.colors.primarybg};
-  flex-direction: column;
-  -webkit-text-stroke: 1px #6560679a;
-`;
-
-//// Tab Achievements info 
-const linkslist = [" ACHIEVEMENTS", " FRIENDS" , " GAME HISTORY"]
-
+//-----//
 const achievment1 = {
   name: "SERGENT",
   desc : "you played 20 game without any loss",
@@ -133,101 +41,14 @@ const achievment3 = {
   desc : "you win 5 game.",
   badge : B1 ,
 }
+var listAchiev = [achievment1 , achievment2 , achievment3,achievment3,achievment3,achievment3,
+  achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,
+  achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,
+  achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment3,
+  achievment1 , achievment2 , achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,
+  achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3]
 
-var listAchiev = [achievment1 , achievment2 , achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment1 , achievment2 , achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment1 , achievment2 , achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment1 , achievment2 , achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3,achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment3, achievment3]
-
-export function TabOne()
-{
-  return (
-    <TabOone >
-      {
-        listAchiev.map((match : any, id : number )=>{
-            return<AchievmentComp key={id}achievment={match}  />
-        })
-      }
-    </TabOone>
-  )
-}
-
-const TabOone = styled.div`
-  /* margin: 20px; */
-  height: 400px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: left;
-  padding: 20px;
-  /* margin: 10px; */
-  overflow-y: scroll;
-
-  &::-webkit-scrollbar {
-  width: 4px;
-}
-
-/* Track */
-&::-webkit-scrollbar-track {
-  background: transparent;
-}
-
-/* Handle */
-&::-webkit-scrollbar-thumb {
-  background: ${props => props.theme.colors.primarybg};
-}
-
-/* Handle on hover */
-&::-webkit-scrollbar-thumb:hover {
-  background: ${props => props.theme.colors.primarybg};
-}
-`;
-
-////// Tab Friends
-
-export function Tabtwo()
-{
-  return (
-    <TabOtwo >
-      {/* <h1> Helloo Tab Friends </h1> */}
-      {
-        listCards.map((match : any, id : number )=>{
-            return<UserCard key={id}data={match}  />
-        })
-      }
-    </TabOtwo>
-  )
-}
-
-const TabOtwo = styled.div` 
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: left;
-  padding: 10px;
-  margin: 0px 0px;
-  overflow-y: scroll;
-  height: auto;
-  max-height: 400px;
-  /* border: 1px solid white; */
-
-  overflow-y: scroll;
-  &::-webkit-scrollbar {
-  width: 4px;
-}
-
-&::-webkit-scrollbar-track {
-  background: transparent; 
-} 
-
-/* Handle */
- &::-webkit-scrollbar-thumb {
-  background: ${props => props.theme.colors.primarybg};
-} 
-
-/* Handle on hover */
- &::-webkit-scrollbar-thumb:hover { 
-  background: ${props => props.theme.colors.primarybg};
-}
-`;
-
-////
-
+  //-----//
 const card = {
   username: "mohammed el-karmi",
   grade : "Yaiba",
@@ -260,27 +81,265 @@ const card4 = {
   status : true ,
   avatar : Badge6,
 }
-
 var listCards = [card, card1,card3, card3, card2,card, card1,card3, card3, card2
-,card, card1,card3, card3, card2,card, card1,card3, card3, card2,card4, card1,card3, card3, card2,card, card1,card3, card4, card2, card, card1,card4, card3, card2,card, card1,card3, card3, card2, card, card1,card3, card3, card2,card, card1,card3, card3, card2, card, card1,card3, card3, card2,card, card1,card3, card3, card2]
-///// Game History Tab
+,card, card1,card3, card3, card2,card, card1,card3, card3, card2,card4, card1,card3,
+card3, card2,card, card1,card3, card4, card2, card, card1,card4, card3, card2,card,
+card1,card3, card3, card2, card, card1,card3, card3, card2,card, card1,card3, card3, 
+card2, card, card1,card3, card3, card2,card, card1,card3, card3, card2]
 
-//////
+//-----//
+const match1 = {
+  match:{
+  name: "Melkarmi",
+  score1 : 7,
+  score2 : 5,
+  img: Badge4,
+  min: 10, 
+  sec: 20}, 
+  isFriend: false,
+}
+const match2 = {
+  match:{
+  name: "Mamali",
+  score1 : 2,
+  score2 : 5,
+  img: Badge3,
+  min: 25, 
+  sec: 29}, 
+  isFriend: true,
+}
+const match3 = {
+  match:{
+  name: "achraf",
+  score1 : 17,
+  score2 : 20,
+  img: Badge5,
+  min: 5, 
+  sec: 20},
+  isFriend: true,
+}
+
+var listGame = [match1 , match2, match3, match1 , match2, match3, match1 , match2, match3,
+   match1 , match2, match3, match1 , match2, match3, match1 , match2, match3, match1 , match2, match3]
+
+export interface GameCompProps {
+    win: boolean
+}
+
+export interface GameCardProps {
+    match: {
+        name: string;
+        score1: number,
+        score2: number,
+        img: any,
+        min: number,
+        sec: number,
+    },
+    isFriend : boolean
+}
+//-----//
+
+//// Default function Profile
+export default function Profile() {
+  return (
+    <div className='container' style={{marginTop: "50px"}}>
+          <TheBox>
+              <PlayerCard player={player} />
+              <ProgressBar>
+                <div >
+                  <div className='lvl'>
+                    level 8 - {ProgressUser}
+                  </div>
+                </div>
+              </ProgressBar>
+          </TheBox>
+          <PlayerTabsBar/>
+    </div>
+  )
+};
+const TheBox = styled.div`
+  padding: 10p;
+  width: 100%;
+  border: 0px solid ${props => props.theme.colors.primarybg};
+`;
+const ProgressBar = styled.div`
+width: 100%;
+height: 32px;
+background-color: ${GreyBackcolor};
+border-radius: 18px;
+display: flex;
+align-items: center;
+margin: 15px 0px;
+
+
+> div{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: flex-end;
+  height: 100%;
+  width: ${ProgressUser};
+  background-color:${Backcolor};
+  border-radius: 15px;
+
+  .lvl{
+    margin-right: 18px;
+    font-size: ${props => props.theme.fontSize.s};
+    font-weight: 12px;
+    color: white;
+    -webkit-text-stroke: 1px #b2a2b69a;
+  }
+
+}
+`
+
+///// PlayerTabs Section
+export function PlayerTabsBar()
+{
+  const [index, setindex] = useState(0)
+  return ( 
+    <PlayerAchieveStyle>
+      <Navlinks  index={index} setindex={(e)=> setindex(e)} list={linkslist}/> 
+        {index === 0 && <TabOne/> }
+        {index === 1 && <Tabtwo/>}
+        {index === 2 && <Tabthree/>}
+        {index === 3 && <Tabfour/>}
+        {index === 4 && <Tabfive/>}
+    </PlayerAchieveStyle>
+  )
+}
+const PlayerAchieveStyle = styled.div`
+  padding-top: 20px;
+  margin : 0px 0px;
+  width:  100%;
+  align-items: center;
+  height: auto;
+  border: 1px solid ${props => props.theme.colors.primarybg};
+  flex-direction: column;
+  -webkit-text-stroke: 1px #6560679a;
+`;
+
+/// To be added : Other users profile // Rooms Page /// All users page // 
+
+export function Tabfour()
+{
+  return (
+    <div> Tab FOUR </div>
+  )
+}
+
+export function Tabfive()
+{
+  return (
+    <div> Tab Five </div>
+  )
+}
+
+//#1  Tab Achievements info 
+const linkslist = [" ACHIEVEMENTS", " FRIENDS" , " GAME HISTORY", "PENDING REQUESTS", "MY BLACK LIST"]
+
+export function TabOne()
+{
+  return (
+    <TabOone >
+      {
+        listAchiev.map((match : any, id : number )=>{
+            return<AchievmentComp key={id}achievment={match}  />
+        })
+      }
+    </TabOone>
+  )
+}
+const TabOone = styled.div`
+  /* margin: 20px; */
+  height: 400px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: left;
+  padding: 20px;
+  /* margin: 10px; */
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+  width: 4px;
+}
+
+/* Track */
+&::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+/* Handle */
+&::-webkit-scrollbar-thumb {
+  background: ${props => props.theme.colors.primarybg};
+}
+
+/* Handle on hover */
+&::-webkit-scrollbar-thumb:hover {
+  background: ${props => props.theme.colors.primarybg};
+}
+`;
+
+//#2  Tab Friends
+export function Tabtwo()
+{
+  return (
+    <TabOtwo >
+      {/* <h1> Helloo Tab Friends </h1> */}
+      {
+        listCards.map((match : any, id : number )=>{
+            return<UserCard key={id}data={match}  />
+        })
+      }
+    </TabOtwo>
+  )
+}
+const TabOtwo = styled.div` 
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: left;
+  padding: 10px;
+  margin: 0px 0px;
+  overflow-y: scroll;
+  height: auto;
+  max-height: 400px;
+  /* border: 1px solid white; */
+
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+  width: 4px;
+}
+
+&::-webkit-scrollbar-track {
+  background: transparent; 
+} 
+
+/* Handle */
+ &::-webkit-scrollbar-thumb {
+  background: ${props => props.theme.colors.primarybg};
+} 
+
+/* Handle on hover */
+ &::-webkit-scrollbar-thumb:hover { 
+  background: ${props => props.theme.colors.primarybg};
+}
+`;
+
+//#3  Game History Tab
+const hieghtTab = "400px";
+
 export function Tabthree()
 {
   return (  
     <TabOthree >
     {
         listGame.map((match : any, id : number )=>{
-            return<GameComp key={id} match={match}  isFriend={true} />
+            return<GameComp key={id} match={match.match}  isFriend={match.isFriend} />
         })
     }
     </TabOthree >
   )
 }
-
-const hieghtTab = "400px";
-
 const TabOthree= styled.div`
   /* background-color: #4a085a4e; */
   width: 100%;
@@ -312,54 +371,7 @@ const TabOthree= styled.div`
     }
 `;
 
-/////// game data
-
-const match1 = {
-  name: "Melkarmi",
-  score1 : 7,
-  score2 : 5,
-  img: Badge4,
-  min: 10, 
-  sec: 20,
-}
-const match2 = {
-  name: "Mamali",
-  score1 : 2,
-  score2 : 5,
-  img: Badge3,
-  min: 25, 
-  sec: 29,
-}
-const match3 = {
-  name: "achraf",
-  score1 : 17,
-  score2 : 20,
-  img: Badge5,
-  min: 5, 
-  sec: 20,
-
-}
-
-var listGame = [match1 , match2, match3, match1 , match2, match3, match1 , match2, match3, match1 , match2, match3, match1 , match2, match3, match1 , match2, match3, match1 , match2, match3]
-
-export interface GameCompProps {
-    win: boolean
-}
-
-export interface GameCardProps {
-    match: {
-        name: string;
-        score1: number,
-        score2: number,
-        img: any,
-        min: number,
-        sec: number,
-    },
-    isFriend : boolean
-}
-
 ////// game Comp
-
 export  function GameComp(props : GameCardProps) {
     var state : boolean =( props.match.score1 > props.match.score2) ? true : false
   return (
