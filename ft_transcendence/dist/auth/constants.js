79691a846e32e04e0853f0be8c8fd616cb8f7976
Achraf BELARIF <57864249+spydersy@users.noreply.github.com>
2022-09-26 20:50:55 +0100

    Delete ft_transcendence directory

--- ft_transcendence/dist/auth/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwtConstants = void 0;
-exports.jwtConstants = {
-    secret: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*()_+',
-};
-//# sourceMappingURL=constants.js.map
