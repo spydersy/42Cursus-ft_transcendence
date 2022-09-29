93b07af11e5df033dfecac4b4cd3fbf7e178982e
spydersy <acbe1325@aol.com>
2022-09-29 10:06:16 +0100

    [NOTE] : reset main branch

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, Res, Param } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  async getUploadedFile(@Param('imgpath') image, @Res() res) {
-    return res.sendFile(image, { root: './upload' });
-  }
-}
--- src/dtos/Inputs.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsString, Matches } from "class-validator";
-
-export class UserName {
-    @IsString()
-    @IsNotEmpty()
-    @Matches(/[a-zA-Z0-9_-]{4,10}/)
-    UserName:   string;
-}
