b06ff10326d846216312abf857946ebcd3eb7935
spydersy <acbe1325@aol.com>
2022-10-30 22:35:43 +0100

    [ADDED] : init script

---
+++ init.sh
@@ -0,0 +1,4 @@
+mkdir app && cd app
+git clone -b back-end https://github.com/spydersy/42Cursus-ft_transcendence .
+git clone -b front-end https://github.com/spydersy/42Cursus-ft_transcendence front-end
+docker-compose up --build
--- src/user/user.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChatModule } from 'src/chat/chat.module';
-import { ChatService } from 'src/chat/chat.service';
-
-@Module({
-    imports: [ChatModule],
-})
-export class UserModule {
-}
