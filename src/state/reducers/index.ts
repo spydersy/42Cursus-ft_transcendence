869c5b9d77b9fed484adebe399825adbd0659065
spydersy <acbe1325@aol.com>
2022-11-01 03:21:54 +0100

    [NOTE] : FIX CONFLICT

--- src/state/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import logginReducer from './loginReducer';
-
-
-const reducers = combineReducers({
-        user: logginReducer,
-});
-
-export default reducers;
